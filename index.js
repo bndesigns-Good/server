b083ad21b279c24db6f87e9e3aaee0eca5c67220
Brennan Newton <brennanmnewton@gmail.com>
2023-11-24 17:18:30 -0600

    Add get user functionality

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ const PORT = process.env.PORT || 8000;
 // Controllers/models
 //
 const getOfferings = (req, res) => {
-    pool.query('SELECT * FROM offerings', (error, offerings) => {
+    pool.query('SELECT * FROM offerings', (error, selection) => {
         if (error) {
             throw error
         }
-        res.status(200).json(offerings.rows)
+        res.status(200).json(selection.rows)
     })
 }
 
 const createOffering = (req, res) => {
     const offeringData = req.body;
-    const queryString = `INSERT INTO offerings (title, category, price, offeree) VALUES ('${offeringData.title}', '${offeringData.category}', ${offeringData.price}, '${offeringData.offeree}')`;
+    const queryString = `INSERT INTO offerings (title, category, price, offeree_id) VALUES ('${offeringData.title}', '${offeringData.category}', ${offeringData.price}, '${offeringData.offeree_id}')`;
     pool.query(queryString, (error, newOffering) => {
             if (error) {
                 throw error
@@ -42,6 +42,18 @@ const deleteOffering = (req, res) => {
     })
 }
 
+const getUser = (req, res) => {
+    const userId = req.params.id;
+    const queryString = `SELECT * FROM users WHERE id=${userId}`;
+    pool.query(queryString, (error, selection) => {
+        if (error) {
+            throw error
+        }
+        const user = selection.rows[0];
+        res.send(user)
+    })
+}
+
 app.use(cors());
 app.use(express.json());
 
@@ -55,6 +67,8 @@ app.get("/", (req, res) => {
 app.get('/offerings', getOfferings)
 app.post('/offerings', createOffering)
 app.delete('/offerings/:id', deleteOffering)
+
+app.get('/user/:id', getUser)
 
 app.get('/message', (req, res) => {
     res.json({ message: "Hello from server!" });
