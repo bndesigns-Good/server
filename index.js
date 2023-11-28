9697e5062316296162321d1c30e118ed4006feb7
Brennan Newton <brennanmnewton@gmail.com>
2023-11-28 14:10:46 -0600

    Get user's offers and Update user

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8000;
 //
 const login = (req, res) => {
     const loginData = req.body;
-    const queryString = `SELECT id, email, password FROM users WHERE email='${loginData.email}' AND password='${loginData.password}'`
+    const queryString = `SELECT id, email, password FROM users WHERE email = '${loginData.email}' AND password = '${loginData.password}';`
     pool.query(queryString, (error, selection) => {
         if (error) {
             throw error
@@ -28,7 +28,18 @@ const login = (req, res) => {
 }
 
 const getOffers = (req, res) => {
-    pool.query('SELECT * FROM offers', (error, selection) => {
+    pool.query('SELECT * FROM offers;', (error, selection) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).send(selection.rows)
+    })
+}
+
+const getUsersOffers = (req, res) => {
+    const userId = req.params.id
+    const queryString = `SELECT * FROM offers WHERE user_id = ${userId};`
+    pool.query(queryString, (error, selection) => {
         if (error) {
             throw error
         }
@@ -37,7 +48,7 @@ const getOffers = (req, res) => {
 }
 
 const getOffersWithUsers = (req, res) => {
-    const queryString = `SELECT id, user_id FROM offers`;
+    const queryString = `SELECT id, user_id FROM offers;`
     pool.query(queryString, (error, selection) => {
         if (error) {
             throw error
@@ -48,7 +59,7 @@ const getOffersWithUsers = (req, res) => {
 
 const createOffer = (req, res) => {
     const offerData = req.body;
-    const queryString = `INSERT INTO offers (title, category, price, user_id) VALUES ('${offerData.title}', '${offerData.category}', ${offerData.price}, '${offerData.user_id}')`;
+    const queryString = `INSERT INTO offers (title, category, price, user_id) VALUES ('${offerData.title}', '${offerData.category}', ${offerData.price}, '${offerData.user_id}');`
     pool.query(queryString, (error, newOffer) => {
             if (error) {
                 throw error
@@ -60,7 +71,7 @@ const createOffer = (req, res) => {
 
 const deleteOffer = (req, res) => {
     const offerId = req.params.id;
-    const queryString = `DELETE FROM offers WHERE id=${offerId}`;
+    const queryString = `DELETE FROM offers WHERE id = ${offerId};`
     pool.query(queryString, (error, deletedOffer) => {
         if (error) {
             throw error
@@ -70,7 +81,7 @@ const deleteOffer = (req, res) => {
 }
 
 const getUsers = (req, res) => {
-    pool.query('SELECT * FROM users', (error, selection) => {
+    pool.query('SELECT * FROM users;', (error, selection) => {
         if (error) {
             throw error
         }
@@ -80,7 +91,7 @@ const getUsers = (req, res) => {
 
 const getUser = (req, res) => {
     const userId = req.params.id;
-    const queryString = `SELECT * FROM users WHERE id=${userId}`;
+    const queryString = `SELECT * FROM users WHERE id = ${userId};`
     pool.query(queryString, (error, selection) => {
         if (error) {
             throw error
@@ -90,9 +101,33 @@ const getUser = (req, res) => {
     })
 }
 
+const updateUser = (req, res) => {
+    const userId = req.params.id;
+    const userData = req.body;
+    // Check to see which data need to be updated
+    const incomingFields = ["name", "pronouns", "bio"]
+    const incomingData = [userData.name, userData.pronouns, userData.bio]
+    let dataString = ""
+    for (let i = 0; i < 3; i++) {
+        if (dataString == "" && incomingData[i] != "") {
+            dataString = dataString + incomingFields[i] + " = '" + incomingData[i] + "'"
+        } else if (dataString != "" && incomingData[i] != "") {
+            dataString = dataString + ", " + incomingFields[i] + " = '" + incomingData[i] + "'"
+        }
+    }
+    const queryString = `UPDATE users SET ${dataString} WHERE id = ${userId};`
+    pool.query(queryString, (error, updatedUser) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).send(updatedUser)
+    })
+    
+}
+
 const createUser = (req, res) => {
     const userData = req.body;
-    const queryString = `INSERT INTO users (email, password, name) VALUES ('${userData.email}', '${userData.password}', '${userData.name}')`;
+    const queryString = `INSERT INTO users (email, password, name) VALUES ('${userData.email}', '${userData.password}', '${userData.name}');`
     pool.query(queryString, (error, newUser) => {
         if (error) {
             throw error
@@ -110,12 +145,14 @@ app.use(express.json());
 app.post('/login', login)
 
 app.get('/offers', getOffers)
+app.get('/offers/:id', getUsersOffers)
 app.get('/offerusers', getOffersWithUsers)
 app.post('/offer', createOffer)
 app.delete('/offer/:id', deleteOffer)
 
 app.get('/users', getUsers)
 app.get('/user/:id', getUser)
+app.patch('/user/:id', updateUser)
 app.post('/user', createUser)
 
 app.listen(8000, () => {
