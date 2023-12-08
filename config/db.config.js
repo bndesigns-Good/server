57b085a17a94caae2dcfb4b3bb9b660c76927a6f
Brennan Newton <brennanmnewton@gmail.com>
2023-12-07 18:05:35 -0600

    Remove space in db config file

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const database = process.env.PGDATABASE;
-
 const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${database}`;
 
 const pool = new Pool({
